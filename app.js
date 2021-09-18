e52b7effe007cdccea4f3fd00c8df5a438dbd184
Roshannair24 <70207465+Roshannair24@users.noreply.github.com>
2021-09-18 19:52:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,9 +13,38 @@ btnTranslate.addEventListener("click", function () {
   console.log("yo clicked");
   console.log("input " + txtInput.value);
 
-  outputDiv.innerText ="fgghh: "+ txtInput.value;
-
-
-
- 
+  outputDiv.innerText = "fgghh: " + txtInput.value;
 });
+
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+const text1 = "i-am-ironman-new";
+const text2 = "i-am-batman-new";
+
+function counstructurl(text) {
+
+
+    console.log("constrictor: "+url + "?"+"text=" + text);
+
+  return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" + '?'+'text=' + text;
+}
+
+function dofetch(text) {
+  fetch(counstructurl(text))
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log(json);
+    });
+}
+
+// dofetch(counstructurl(text1));
+
+
+
+
+
+
+
+
